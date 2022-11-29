44e01aaa9a390444979dc2f6e197820d1e59a555
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-11-29 08:22:36 +0600

    card width edited

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -132,7 +132,7 @@ const Sidebar = ({ hide }) => {
 
       {/* User Card */}
       <div
-        className="rounded-lg py-2 px-4 mx-6 mt-32 mb-8"
+        className="rounded-lg py-2 px-4 mx-6 mt-52 mb-8"
         style={{
           background: "rgba(34, 40, 44, 0.7)",
           boxShadow: "0px 4px 30px rgba(0, 0, 0, 0.21)",
